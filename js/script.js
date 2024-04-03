6b12d196d75d8a9558f8e10529b580738899ba37
Andreasgura <andre-56@hotmail.it>
2024-04-03 11:12:56 +0200

    init + basic structure

---
+++ js/script.js
@@ -0,0 +1,16 @@
+// import { toDo } from "./data.js";
+
+const {createApp} = Vue;
+createApp({
+    data() {
+      return {
+        
+      }
+    },
+    methods : {
+     
+      },
+  mounted () { 
+                     
+    }
+  }).mount('#app');
