7e1db40402d3f2cda72c1b4ac3a8d8c16664c3a9
Andreasgura <andre-56@hotmail.it>
2024-04-04 19:02:16 +0200

    milestone 3

--- js/script.js
+++ js/script.js
@@ -8,20 +8,50 @@ createApp({
       return {
         contacts,
         activeContactId : 1,
+        textMessage : ''
       }
     },
     methods : {
       selectContact(id){
-        this.activeContactId = this.contacts.findIndex((el)=> el.id === id)
+        this.activeContactId = id
         console.log(this.activeContactId);
-        return this.activeContactId
-      } 
+      },
+      newMessage(){
+        const lastMessage =  {
+         date: '10/01/2020 15:30:55',
+         message: this.textMessage,
+         status: 'sent'
+        }
+        this.activeContact.messages.push(lastMessage)
+        this.textMessage = ''
+      }
       },
     computed : {
       activeContact(){
-        return this.contacts.find((el)=> el.id === this.activeContactId) 
-        },
 
+      /*   let activeContact = null;
+        this.contacts.forEach(contact => {
+          
+        });
+
+      function find (array, funzioneInput){
+        let result = null;
+          for (let i = 0; i<array.length; ++i){
+            if (funzioneInput(array[i])){
+              result = array[i];
+              break;
+            }
+          }
+          return result;
+      }
+
+      function funzioneInput (contact){
+        contact.id === 5;
+      } */
+
+        return this.contacts.find(  (contact) =>  contact.id === this.activeContactId ) 
+        },
+      
     },
     mounted () { 
             
